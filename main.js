2ac0ff3c395707e71535f53b4d9cc54400d0d237
emmamaciuca <146767596+emmamaciuca@users.noreply.github.com>
2024-02-26 16:04:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,365 @@
+var descPartideScurte = {};
+descPartideScurte["PSD"] = 'Partidul Social Democrat este cel mai mare partid politic din Romania. Ideologia principala a partidului este social-democratia iar pozitia politica este de centru-stanga. PSD este condus in prezent de Marcel Ciolacu, secretarul general fiind Paul Stanescu. Din 2021, PSD se afla la guvernare alaturi de PNL in cadrul CNR (Coalitia Nationala pentru Romania).';
+descPartideScurte["PNL"] = "Partidul National Liberal este cel mai vechi partid politic din Romania, fiind infiintat pentru prima oara in 1875. Pozitia politica a partidului este de centru-dreapta, ideologia principala fiind liberalismul.Alaturi de PSD, face parte din CNR (Coalitia Nationala pentru Romania) si este la guvernare in cadrul acesteia din 2021. Presedintele PNL este Nicolae Ciuca iar secretarul general al partidului este Lucian Bode.";
+descPartideScurte["USR"] = "Uniunea Salvati Romania este al treilea cel mai mare partid politic in Parlamentul Romaniei. Ideologia principala a partidului este liberalismul, fiind un partid de centru-dreapta modern. USR sustine parteneriatul civil si a candidat in trecut pe o platforma anticoruptie. De asemenea, partidul sustine parcursul european al Republicii Moldova. Presedintele partidului este Catalin Drula.";
+descPartideScurte["UDMR"] = "Uniunea Democrata Maghiara din Romania este o organizatie politica din Romania fondata cu scopul de a reprezenta interesele comunitatii maghiare. A fost infiintata pe 25 decembrie 1989, ziua in care Nicolae Ceausescu a fost executat.Fiind membra PPE, pozitia politica a uniunii este de centru-dreapta. Organizatia lupta pentru autonomie teritoriala. A facut de asemena parte din CNR alaturi de PSD si PNL, coalitie din care nu mai face parte in prezent.";
+descPartideScurte["AUR"] = "Alianta pentru Unirea Romanilor este un partid politic fondat in 2019. Este un partid nationalist, conservator, pozitia lui politica fiind de dreapta (unii considera ca este de extrema dreapta). Unul dintre obiectivele principale ale partidului este unirea Romaniei cu Basarabia. Valorile partidului, asa cum sunt descrise pe site-ul lor, sunt familia, natiunea, credinta crestina si libertatea.  Liderul partidului este George Simion iar Cristian David este secretar general.";
+
+var descPartideLungi = {};
+descPartideLungi["PSD"] = 'Partidul Social Democrat este cel mai mare partid politic din Romania. Ideologia principala a partidului este social-democratia iar pozitia politica este de centru-stanga. PSD este condus in prezent de Marcel Ciolacu, secretarul general fiind Paul Stanescu. Din 2021, PSD se afla la guvernare alaturi de PNL in cadrul CNR (Coalitia Nationala pentru Romania). Pentru mai multe informatii, puteti accesa linkurile urmatoare:</p><p><a href="https://ro.wikipedia.org/wiki/Partidul_Social_Democrat_(România)" target="_blank">Partidul Social Democrat</a></p><p><a href="https://ro.wikipedia.org/wiki/Social-democrație" target="_blank">Democratie sociala</a></p><p><a href="https://ro.wikipedia.org/wiki/Coaliția_Națională_pentru_România" target="_blank">Coalitia Nationala pentru Romania</a></p><p><a href="https://www.psd.ro" target="_blank">PSD</a>';
+descPartideLungi["PNL"] = 'Partidul National Liberal este cel mai vechi partid politic din Romania, fiind infiintat pentru prima oara in 1875. Pozitia politica a partidului este de centru-dreapta, ideologia principala fiind liberalismul.Alaturi de PSD, face parte din CNR (Coalitia Nationala pentru Romania) si este la guvernare in cadrul acesteia din 2021. Presedintele PNL este Nicolae Ciuca iar secretarul general al partidului este Lucian Bode. Pentru mai multe informatii, puteti accesa linkurile urmatoare:</p><p><a href="https://ro.wikipedia.org/wiki/Partidul_Național_Liberal_(România)" target="_blank">Partidul National Liberal</a></p><p><a href="https://ro.wikipedia.org/wiki/Liberalism" target="_blank">Liberalism</a></p><p><a href="https://ro.wikipedia.org/wiki/Coaliția_Națională_pentru_România" target="_blank">Coalitia Nationala pentru Romania</a></p><p><a href="https://ro.wikipedia.org/wiki/Liberalismul_românesc" target="_blank">Liberalismul romanesc</a></p><p><a href="https://pnl.ro" target="_blank">PNL</a>';
+descPartideLungi["USR"] = 'Uniunea Salvati Romania este al treilea cel mai mare partid politic in Parlamentul Romaniei. Ideologia principala a partidului este liberalismul, fiind un partid de centru-dreapta modern. USR sustine parteneriatul civil si a candidat in trecut pe o platforma anticoruptie. De asemenea, partidul sustine parcursul european al Republicii Moldova. Presedintele partidului este Catalin Drula. Pentru mai multe informatii, puteti accesa linkurile urmatoare:</p><p><a href="https://ro.wikipedia.org/wiki/Uniunea_Salvați_România#Republica_Moldova" target="_blank">Uniunea Salvati Romania</a></p><p><a href="https://ro.wikipedia.org/wiki/Liberalism" target="_blank">Liberalism</a></p><p><a href="https://ro.wikipedia.org/wiki/Europenism" target="_blank">Europenism</a></p><p><a href="https://usr.ro" target="_blank">USR</a>';
+descPartideLungi["UDMR"] = 'Uniunea Democrata Maghiara din Romania este o organizatie politica din Romania fondata cu scopul de a reprezenta interesele comunitatii maghiare. A fost infiintata pe 25 decembrie 1989, ziua in care Nicolae Ceausescu a fost executat.Fiind membra PPE (Partidul Popular European), pozitia politica a uniunii este de centru-dreapta. Organizatia lupta pentru autonomie teritoriala. A facut de asemena parte din CNR (Coalitia Nationala pentru Romania) alaturi de PSD si PNL, coalitie din care nu mai face parte in prezent. Pentru mai multe informatii, puteti accesa linkurile urmatoare:</p><p><a href="https://ro.wikipedia.org/wiki/Uniunea_Democrată_Maghiară_din_România " target="_blank">Uniunea Democrata Maghiara din Romania</a></p><p><a href="https://ro.wikipedia.org/wiki/Partidul_Popular_European" target="_blank">Partidul Popular European</a></p><p><a href="http://udmr.ro" target="_blank">UDMR</a>';
+descPartideLungi["AUR"] = 'Alianta pentru Unirea Romanilor este un partid politic fondat in 2019. Este un partid nationalist, conservator, pozitia lui politica fiind de dreapta (unii considera ca este de extrema dreapta). Unul dintre obiectivele principale ale partidului este unirea Romaniei cu Basarabia. Valorile partidului, asa cum sunt descrise pe site-ul lor, sunt familia, natiunea, credinta crestina si libertatea.  Liderul partidului este George Simion iar Cristian David este secretar general. Pentru mai multe informatii, puteti accesa linkurile urmatoare:</p><p><a href="https://ro.wikipedia.org/wiki/Alianța_pentru_Unirea_Românilor" target="_blank">Alianta pentru Unirea Romanilor</a></p><p><a href="https://ro.wikipedia.org/wiki/Naționalism_românesc" target="_blank">Nationalism romanesc</a></p><p><a href="https://ro.wikipedia.org/wiki/Unirea_Republicii_Moldova_cu_România" target="_blank">Unirea Republicii Moldova cu Romania</a></p><p><a href="https://partidulaur.ro" target="_blank">Partidul AUR</a>';
+
+
+var partidSelectat = 0;
+var items;
+
+const maxPsd = 7;
+const maxAur = 4;
+const maxPnl = 6;
+const maxUsr = 6;
+const maxUdmr = 3;
+
+var maxDic = {};
+maxDic["PSD"]=maxPsd;
+maxDic["PNL"]=maxPnl;
+maxDic["USR"]=maxUsr;
+maxDic["AUR"]=maxAur;
+maxDic["UDMR"]=maxUdmr;
+
+var dict = {};
+
+
+let currentQuestion = 0;
+let score = [];
+let selectedAnswersData = [];
+const totalQuestions =questions.length;
+
+const startMenu = document.querySelector('.inceput');
+const container = document.querySelector('.quiz-container');
+const questionEl = document.querySelector('.question');
+const option1 = document.querySelector('.option1');
+const option2 = document.querySelector('.option2');
+const option3 = document.querySelector('.option3');
+const nextButton = document.querySelector('.next');
+//const endButton = document.querySelector('.endButton');
+const startButton = document.querySelector('.startButton');
+const result = document.querySelector('.result');
+const moreInfo = document.querySelector('.moreInfo');
+
+//Function to generate question 
+function generateQuestions (index) {
+    //Select each question by passing it a particular index
+    const question = questions[index];
+    const option1Total = questions[index].answer1Total;
+    const option2Total = questions[index].answer2Total;
+    const option3Total = questions[index].answer3Total;
+
+    //Populate html elements 
+    questionEl.innerHTML = `${index + 1}. ${question.question}`
+    option1.setAttribute('data-total', `${option1Total}`);
+    option2.setAttribute('data-total', `${option2Total}`);
+    option3.setAttribute('data-total', `${option3Total}`);
+    option1.innerHTML = `${question.answer1}`
+    option2.innerHTML = `${question.answer2}`
+    option3.innerHTML = `${question.answer3}`
+}
+
+
+function loadNextQuestion () {
+    const selectedOption = document.querySelector('input[type="radio"]:checked');
+    //Check if there is a radio input checked
+    if(!selectedOption) {
+        alert('Te rog selecteaza un raspuns!');
+        return;
+    }
+    //Get value of selected radio
+    const answerScore = selectedOption.nextElementSibling.getAttribute('data-total');
+    //console.log(answerScore);
+    ////Add the answer score to the score array
+    score.push(answerScore);
+
+    selectedAnswersData.push(selectedOption.nextElementSibling.innerHTML);
+    
+
+    //const totalScore = score.reduce((total, currentNum) => total + currentNum);
+
+
+    //Finally we incement the current question number ( to be used as the index for each array)
+    currentQuestion++;
+
+        //once finished clear checked
+        selectedOption.checked = false;
+    //If quiz is on the final question
+    if(currentQuestion == totalQuestions - 1) {
+        nextButton.textContent = 'Final';
+    }
+    //If the quiz is finished then we hide the questions container and show the results 
+    if(currentQuestion == totalQuestions) {
+        loadEndMenu();
+        return;
+    }
+    generateQuestions(currentQuestion);
+}
+
+//Fuction to reset and restart the quiz;
+function restartQuiz(e) {
+    if(e.target.matches('button')) {
+        //reset array index and score
+        currentQuestion = 0;
+        score = [];
+        //Reload quiz to the start
+        location.reload();
+    }
+
+}
+
+function startQuiz(){
+    container.style.display = 'flex';
+    startMenu.style.display = 'none';
+}
+
+function loadEndMenu(){
+    var psd = 0, pnl = 0, usr = 0, aur = 0, udmr = 0, i = 0, j = 0;
+    for(i = 0; i < totalQuestions; i++)
+    {
+        if(score.length <= i)
+            break;
+        var s = score[i].split(' ');
+        for(j = 0; j < s.length; j++)
+        {
+            if(s[j] == "psd")
+                psd++;
+            if(s[j]=="pnl")
+                pnl++;
+            if(s[j]=="aur")
+                aur++;
+            if(s[j]=="udmr")
+                udmr++;
+            if(s[j]=="usr")
+                usr++;   
+        }
+
+    }
+    console.log("PSD: " + psd);
+    console.log("AUR: " + aur);
+    console.log("UDMR: " + udmr);
+    console.log("USR: " + usr);
+    console.log("PNL: " + pnl);
+
+    dict["USR"]=(100*usr/maxUsr).toFixed(2);
+    dict["PNL"]=(100*pnl/maxPnl).toFixed(2);
+    dict["PSD"]=(100*psd/maxPsd).toFixed(2);
+    dict["AUR"]=(100*aur/maxAur).toFixed(2);
+    dict["UDMR"]=(100*udmr/maxUdmr).toFixed(2);
+
+    culoarePartid={};
+    culoarePartid["PNL"]='rgba(249, 231, 0, 1)';
+    culoarePartid["PSD"]='rgba(237, 38, 43, 1)';
+    culoarePartid["USR"]='rgba(0, 42, 89, 1)';
+    culoarePartid["UDMR"]='rgba(21, 128, 60, 1)';
+    culoarePartid["AUR"]='rgba(252, 194, 36, 1)';
+
+    items = Object.keys(dict).map(function(key){
+        return [key, dict[key], culoarePartid[key]];
+    });
+
+    items.sort(function(first, second){
+        return second[1]-first[1];
+    });
+
+    console.log(items);
+    
+    partid=[];
+
+    for(i=0; i<items.length; i++)
+        partid[i]=items[i][0]+": "+items[i][1]+"%";
+
+    numePartideSortat=[]
+    for(i=0; i<items.length; i++)
+        numePartideSortat[i]=items[i][0];
+    valoriPartideSortat=[]
+    for(i=0; i<items.length; i++)
+        valoriPartideSortat[i]=items[i][1];
+    culoriPartideSortat=[]
+    for(i=0; i<items.length; i++)
+        culoriPartideSortat[i]=items[i][2];
+
+    moreInfo.style.display = 'none';
+    startMenu.style.display = 'none';
+    container.style.display = 'none';
+    result.style.display = 'flex';
+    result.innerHTML =
+        `
+        <div class="resultrow">
+            <div>
+                <h3>${(items[0][0])}</h3>
+                <p style="font-size:20px">${(descPartideScurte[items[0][0]])}</p>
+                <button class="butonpartid1">Descopera mai multe</button>
+            </div>
+            <div>
+                <canvas id="myChart" style="width:100%;"></canvas>
+                <button class="restart">Restart</button>
+            </div>
+            <div>
+                <h3>${(items[1][0])}</h3>
+                <p style="font-size:20px">${(descPartideScurte[items[1][0]])}</p>
+                <button class="butonpartid2">Descopera mai multe</button>
+            </div>
+            <div>
+                <h3>${(items[2][0])}</h3>
+                <p style="font-size:20px">${(descPartideScurte[items[2][0]])}</p>
+                <button class="butonpartid3">Descopera mai multe</button>
+            </div>
+            <div>
+                <h3>${(items[3][0])}</h3>
+                <p style="font-size:20px">${(descPartideScurte[items[3][0]])}</p>
+                <button class="butonpartid4">Descopera mai multe</button>
+            </div>
+            <div>
+                <h3>${(items[4][0])}</h3>
+                <p style="font-size:20px">${(descPartideScurte[items[4][0]])}</p>
+                <button class="butonpartid5">Descopera mai multe</button>
+            </div>
+        </div>
+        `;
+
+    document.querySelector('.butonpartid1').addEventListener('click',butonPartid0);
+    document.querySelector('.butonpartid2').addEventListener('click',butonPartid1);
+    document.querySelector('.butonpartid3').addEventListener('click',butonPartid2);
+    document.querySelector('.butonpartid4').addEventListener('click',butonPartid3);
+    document.querySelector('.butonpartid5').addEventListener('click',butonPartid4);
+
+    const restartButton = document.querySelector('.restart');
+    restartButton.addEventListener('click',restartQuiz);
+    makeGraph(numePartideSortat, valoriPartideSortat, culoriPartideSortat);
+}
+
+//100% exista o metoda mai buna de a face asta, dar nu am gasit-o
+function butonPartid0(){
+    partidSelectat=0;
+    moreInformation();
+}
+
+function butonPartid1(){
+    partidSelectat=1;
+    moreInformation();
+}
+
+function butonPartid2(){
+    partidSelectat=2;
+    moreInformation();
+}
+
+function butonPartid3(){
+    partidSelectat=3;
+    moreInformation();
+}
+
+function butonPartid4(){
+    partidSelectat=4;
+    moreInformation();
+}
+
+function moreInformation(){
+    console.log(partidSelectat);
+    moreInfo.style.display = 'flex';
+
+    var questionListDisplay = "";
+
+    console.log("Score: " + score.length + " " + totalQuestions.length);
+
+    var count=0;
+
+    for(var i=0; i<totalQuestions; i++){
+        if(score.length <= i)
+            break;
+        const question = questions[i];
+        var ok = 0;
+        var s = score[i].split(' ');
+        console.log("Hahahahi" + s);
+        for(j = 0; j < s.length; j++)
+        {
+            if(s[j]==items[partidSelectat][0].toLowerCase())
+            {
+                ok=1;
+                console.log("Hihihiha");
+            }
+        }
+        if(ok==1){
+            count++;
+            questionListDisplay += ("<p>" + (i+1) + ". " + question.question + " " + selectedAnswersData[i] + "</p>");
+        }
+    }
+
+    console.log(selectedAnswersData);
+
+    moreInfo.innerHTML =
+     `
+     <div class="title">${(items[partidSelectat][0])}</div>
+        <p>${(descPartideLungi[items[partidSelectat][0]])}</p>
+        <div class="intrebariNimerite">
+            <h3>Intrebarile unde ai fost de acord cu ${(items[partidSelectat][0])} (${count}/${maxDic[items[partidSelectat][0]]}):</h3>
+            ${questionListDisplay}
+        </div>
+        <button class="backToResult">Inapoi</button>
+     `;
+
+    result.style.display = 'none';
+    const backToResultButton = document.querySelector('.backToResult');
+    backToResultButton.addEventListener('click',loadEndMenu);
+}
+
+function makeGraph(numePartideSortat, valoriPartideSortat, culoriPartideSortat){
+
+    Chart.defaults.global.defaultFontColor = "#3B413C";
+    Chart.defaults.global.defaultFontFamily='Roboto';
+    Chart.defaults.global.defaultFontSize=24;
+
+    const datas = {
+        labels: numePartideSortat,
+        datasets: [{
+            label: 'Rezultat',
+            data: valoriPartideSortat,
+            backgroundColor: culoriPartideSortat,
+            borderColor: [
+                'rgb(0, 0, 0)',
+                'rgb(0, 0, 0)',
+                'rgb(0, 0, 0)',
+                'rgb(0, 0, 0)',
+                'rgb(0, 0, 0)'
+              ],
+              borderWidth: 2
+        }]
+    };
+        
+    new Chart("myChart", {
+        type: "bar",
+        data: datas,
+        options: {
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0,
+                        max: 100
+                    },
+                    gridLines: { color: "#fff" }
+                }],
+                xAxes: [{gridLines: { color: "#fff" }}]
+            }
+          }
+    });
+}
+
+//startMenu.style.display = 'none';
+moreInfo.style.display = 'none';
+result.style.display = 'none';
+container.style.display = 'none';
+generateQuestions(currentQuestion);
+nextButton.addEventListener('click', loadNextQuestion);
+//endButton.addEventListener('click', loadEndMenu);
+startButton.addEventListener('click', startQuiz);
---
+++ questions.js
@@ -0,0 +1,83 @@
+const questions = [
+  {
+    "question": "Crezi ca Romania ar trebui sa aiba o economie mai liberala?",
+    "answer1": "Da",
+    "answer1Total": "pnl usr",
+    "answer2": "Nu",
+    "answer2Total": "psd",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+  {
+    "question": "Esti de acord cu promovarea intereselor minoritatilor etnice in politica romaneasca?",
+    "answer1": "Da",
+    "answer1Total": "udmr",
+    "answer2": "Nu",
+    "answer2Total": "aur",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+  {
+    "question": "Esti de acord cu promovarea valorilor conservatoare in societate?",
+    "answer1": "Da",
+    "answer1Total": "aur pnl",
+    "answer2": "Nu",
+    "answer2Total": "usr",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+  {
+    "question": "Ar trebui ca statul sa fie mai implicat in economie?",
+    "answer1": "Da",
+    "answer1Total": "psd",
+    "answer2": "Nu",
+    "answer2Total": "pnl usr",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+  {
+    "question": "Esti de acord cu unirea Romaniei si a Republicii Moldova?",
+    "answer1": "Da",
+    "answer1Total": "aur psd",
+    "answer2": "Nu",
+    "answer2Total": "",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+  {
+    "question": "Ar trebui schimbarea climei sa fie de o importanta primara pentru stat?",
+    "answer1": "Da",
+    "answer1Total": "udmr usr",
+    "answer2": "Nu",
+    "answer2Total": "pnl psd",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+  {
+    "question": "Esti de acord cu faptul ca Romania ar trebui sa aiba mai multe programe sociale pentru persoanele defavorizate?",
+    "answer1": "Da",
+    "answer1Total": "psd",
+    "answer2": "Nu",
+    "answer2Total": "pnl",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+  {
+    "question": "Sunteti de acord cu parteneriatul civil?",
+    "answer1": "Da",
+    "answer1Total": "usr",
+    "answer2": "Nu",
+    "answer2Total": "psd aur",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+  {
+    "question": "Ar trebui ca renuntarea la carbune sa fie o prioritate pentru stat?",
+    "answer1": "Da",
+    "answer1Total": "",
+    "answer2": "Nu",
+    "answer2Total": "psd pnl udmr usr",
+    "answer3": "Ma abtin",
+    "answer3Total": ""
+  },
+]
